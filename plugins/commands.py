f3a0830ad810e9688ae36578294097d65b181ae6
Physic_Hybrid <85246569+PhysicHybridTG@users.noreply.github.com>
2021-07-28 21:27:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -100,7 +100,7 @@ async def start(bot, cmd):
                 [
                     [
                         InlineKeyboardButton("Search Here🔎", switch_inline_query_current_chat=''),
-                        InlineKeyboardButton("Channel 🎥", url="https://t.me/TVseriesLand4U")
+                        InlineKeyboardButton("Group 🍿", url="https://t.me/SeriesLandChat")
                     ],
                     [
                         InlineKeyboardButton('MYdev👩‍💻', url='https://t.me/Physic_hybrid'),
@@ -194,8 +194,8 @@ async def delete(bot, message):
 async def bot_info(bot, message):
     buttons = [
         [
-            InlineKeyboardButton('Update Channel', url='https://t.me/subin_works'),
-            InlineKeyboardButton('Source Code', url='https://github.com/subinps/Media-Search-bot')
+            InlineKeyboardButton('Channel🎥', url='https://t.me/TVseriesLand4U'),
+            InlineKeyboardButton('Group 🍿', url='https://t.me/SeriesLandChat')
         ]
         ]
     await message.reply(text="<b>Developer : <a href='https://t.me/subinps_bot'>SUBIN</a>\nLanguage : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nSource Code : <a href='https://github.com/subinps/Media-Search-bot'>Click here</a>\nUpdate Channel : <a href='https://t.me/subin_works'>XTZ Bots</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
