908b8f39ee5fcfff8212cb9a587fa8df9bb411c5
Physic_Hybrid <85246569+PhysicHybridTG@users.noreply.github.com>
2021-08-09 12:21:32 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -27,7 +27,7 @@ async def start(bot, cmd):
                 ident, file_id = cmd.text.split("_-_-_-_")
                 await bot.send_message(
                     chat_id=cmd.from_user.id,
-                    text="**Please Join My Updates Channel to use this Bot!**",
+                    text="**Hey..Bruh🙋‍♂️..Please Join My Updates Channel to use this Bot!**",
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
