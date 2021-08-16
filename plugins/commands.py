dde3f71cb43321629b62c04dfadcf87ad352246a
SUBIN <64341611+subinps@users.noreply.github.com>
2021-08-16 21:00:48 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -92,8 +92,9 @@ async def start(bot, cmd):
             )
         )
     else:
-        await cmd.reply_text(
-            text=f"<b>𝐘𝐨..𝐘𝐨..{cmd.from_user.mention} 🙋, 𝐈'𝐦 𝐏𝐨𝐰𝐞𝐫𝐟𝐮𝐥 𝐌𝐞𝐝𝐢𝐚 𝐒𝐞𝐚𝐫𝐜𝐡 𝐁𝐨𝐭 𝐨𝐫 𝐲𝐨𝐮 𝐜𝐚𝐧 𝐜𝐚𝐥𝐥 𝐦𝐞 𝐚𝐬 𝐀𝐮𝐭𝐨-𝐅𝐢𝐥𝐭𝐞𝐫 𝐁𝐨𝐭\n𝐇𝐞𝐫𝐞 𝐲𝐨𝐮 𝐜𝐚𝐧 𝐬𝐞𝐚𝐫𝐜𝐡 𝐟𝐢𝐥𝐞𝐬 𝐢𝐧 𝐈𝐧𝐥𝐢𝐧𝐞 𝐦𝐨𝐝𝐞 𝐚𝐬 𝐰𝐞𝐥𝐥 𝐚𝐬 𝐏𝐌, 𝐔𝐬𝐞 𝐭𝐡𝐞 𝐛𝐞𝐥𝐨𝐰 𝐛𝐮𝐭𝐭𝐨𝐧𝐬 𝐭𝐨 𝐬𝐞𝐚𝐫𝐜𝐡 𝐟𝐢𝐥𝐞𝐬 𝐨𝐫 𝐬𝐞𝐧𝐝 𝐦𝐞 𝐭𝐡𝐞 𝐧𝐚𝐦𝐞 𝐨𝐟 𝐟𝐢𝐥𝐞 𝐭𝐨 𝐬𝐞𝐚𝐫𝐜𝐡©️MᴀɪɴᴛᴀɪɴᴇD Bʏ  ᴘʜʏsɪᴄ_ʜʏʙʀɪᴅ🇵🇹/🇦🇪 (https://t.me/Physic_hybrid)</b>",
+        await cmd.reply_photo(
+            photo="https://telegra.ph/file/bc0c97a91d28a93c8c4fe.jpg",
+            caption=f"<b>𝐘𝐨..𝐘𝐨..{cmd.from_user.mention} 🙋, 𝐈'𝐦 𝐏𝐨𝐰𝐞𝐫𝐟𝐮𝐥 𝐌𝐞𝐝𝐢𝐚 𝐒𝐞𝐚𝐫𝐜𝐡 𝐁𝐨𝐭 𝐨𝐫 𝐲𝐨𝐮 𝐜𝐚𝐧 𝐜𝐚𝐥𝐥 𝐦𝐞 𝐚𝐬 𝐀𝐮𝐭𝐨-𝐅𝐢𝐥𝐭𝐞𝐫 𝐁𝐨𝐭\n𝐇𝐞𝐫𝐞 𝐲𝐨𝐮 𝐜𝐚𝐧 𝐬𝐞𝐚𝐫𝐜𝐡 𝐟𝐢𝐥𝐞𝐬 𝐢𝐧 𝐈𝐧𝐥𝐢𝐧𝐞 𝐦𝐨𝐝𝐞 𝐚𝐬 𝐰𝐞𝐥𝐥 𝐚𝐬 𝐏𝐌, 𝐔𝐬𝐞 𝐭𝐡𝐞 𝐛𝐞𝐥𝐨𝐰 𝐛𝐮𝐭𝐭𝐨𝐧𝐬 𝐭𝐨 𝐬𝐞𝐚𝐫𝐜𝐡 𝐟𝐢𝐥𝐞𝐬 𝐨𝐫 𝐬𝐞𝐧𝐝 𝐦𝐞 𝐭𝐡𝐞 𝐧𝐚𝐦𝐞 𝐨𝐟 𝐟𝐢𝐥𝐞 𝐭𝐨 𝐬𝐞𝐚𝐫𝐜𝐡©️MᴀɪɴᴛᴀɪɴᴇD Bʏ  ᴘʜʏsɪᴄ_ʜʏʙʀɪᴅ🇵🇹/🇦🇪 (https://t.me/Physic_hybrid)</b>",
             parse_mode="Markdown",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
